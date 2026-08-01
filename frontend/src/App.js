7bb2bde06536efe8e7fe6679e431886c4dfdc77b
Meghana Reddy Nandyala <51616488+Meghanareddy13@users.noreply.github.com>
2026-08-01 15:52:13 -0400

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import ProductList from './ProductList';
+
+function App() {
+  return (
+    <div className="app-layout">
+      <h1>Product Inventory</h1>
+      <ProductList />
+    </div>
+  );
+}
+export default App;
